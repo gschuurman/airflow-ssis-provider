08738b37ff4f03734e4347a57ce7bb1122dba3db
Glenn Schuurman <gschuurman@scamander.com>
2024-06-24 16:43:41 +0200

    Bump Version to 1.0.3

--- src/SSIS_Operator/sensors/ssis_package_sensor.py
+++ src/SSIS_Operator/sensors/ssis_package_sensor.py
@@ -1,6 +1,10 @@
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
 from airflow.sensors.base import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from airflow.utils.context import Context
 
 
 class PackageExecutionError(Exception):
@@ -47,7 +51,7 @@ class SsisPackageSensor(BaseSensorOperator):
         self.parameters = parameters
         self.xcom_task_id = xcom_task_id
 
-    def poke(self, context):
+    def poke(self, context: Context):
         hook = MsSqlHook(
             mssql_conn_id=self.conn_id,
             schema=self.database
